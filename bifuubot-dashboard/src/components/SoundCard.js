3785475fd7a1d91a31c23db994c4cd514da1e068
Dustin Stover <DustinStover@gmail.com>
2020-05-30 23:45:27 -0400

    CRUD of Sounds!

--- bifuubot-dashboard/src/components/SoundCard.js
+++ bifuubot-dashboard/src/components/SoundCard.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { makeStyles, Card, CardContent, IconButton } from '@material-ui/core';
 import DeleteIcon from '@material-ui/icons/Delete';
 import PlayArrowIcon from '@material-ui/icons/PlayArrow';
 import Edit from '@material-ui/icons/Edit';
+
+import { storage, db } from '../services/firebase';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -23,20 +25,67 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 const SoundCard = (props) => {
+  const [audio, setAudio] = useState(new Audio());
+  const [edit, setEdit] = useState(false);
+
+  const editNameField = React.createRef();
+
+  const updateName = async () => {
+    if (editNameField.current.value === props.data.name) return setEdit(false);
+
+    await db.collection('sounds').doc(props.id).update({
+      name: editNameField.current.value,
+    });
+
+    setEdit(false);
+  };
+
+  const editField = () => {
+    return (
+      <div>
+        <input type="text" ref={editNameField} defaultValue={props.data.name} />
+        <button onClick={updateName}>Submit</button>
+        <button onClick={() => setEdit(false)}>Cancel</button>
+      </div>
+    );
+  };
+
+  useEffect(() => {
+    const getAudioURL = async () => {
+      const url = await storage.ref(props.data.storagePath).getDownloadURL();
+      setAudio(new Audio(url));
+    };
+    getAudioURL();
+  }, [props.data]);
+
   const classes = useStyles();
+
   return (
     <Card className={classes.root}>
       <CardContent className={classes.content}>
-        {props.data.name}
-        <IconButton>
+        {edit ? editField() : props.data.name}
+        <IconButton
+          onClick={() => {
+            audio.play();
+          }}
+        >
           <PlayArrowIcon />
         </IconButton>
       </CardContent>
       <div className={classes.controls}></div>
-      <IconButton>
+      <IconButton
+        onClick={() => {
+          setEdit(!edit);
+        }}
+      >
         <Edit />
       </IconButton>
-      <IconButton>
+      <IconButton
+        onClick={() => {
+          db.collection('sounds').doc(props.id).delete();
+          storage.ref(props.data.storagePath).delete();
+        }}
+      >
         <DeleteIcon />
       </IconButton>
     </Card>
@@ -44,7 +93,7 @@ const SoundCard = (props) => {
 };
 
 SoundCard.propTypes = {
-  name: PropTypes.object.isRequired,
+  data: PropTypes.object.isRequired,
 };
 
 export default SoundCard;
--- bifuubot-dashboard/src/pages/Sounds.js
+++ bifuubot-dashboard/src/pages/Sounds.js
@@ -1,6 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import { Card, CardContent, IconButton } from '@material-ui/core';
-import { DeleteIcon } from '@material-ui/icons/Delete';
 import { upload } from '../helpers/storage';
 import { db } from '../services/firebase';
 import SoundCard from '../components/SoundCard';
@@ -11,12 +9,6 @@ const Sounds = () => {
   const soundName = React.createRef();
 
   useEffect(() => {
-    // const getSoundNames = async () => {
-    //   const soundsFromDB = await db.collection(`sounds`).get();
-    //   setSounds(soundsFromDB);
-    // };
-    // console.log('effect');
-    // getSoundNames();
     db.collection('sounds').onSnapshot((snapshot) => {
       setSounds(snapshot);
     });
@@ -36,7 +28,7 @@ const Sounds = () => {
   const renderListOfSounds = () => {
     let list = [];
     sounds.forEach((doc) => {
-      const sound = <SoundCard data={doc.data()} />;
+      const sound = <SoundCard data={doc.data()} key={doc.id} id={doc.id} />;
       list.push(sound);
     });
     return list;
