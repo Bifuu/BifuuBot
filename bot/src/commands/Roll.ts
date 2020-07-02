be55629449b2a16160c063a6c7424478dadb4ba8
Dustin Stover <DustinStover@gmail.com>
2020-07-02 12:35:07 -0400

    removed the troll

--- bot/src/commands/Roll.ts
+++ bot/src/commands/Roll.ts
@@ -6,9 +6,7 @@ const Roll: ICommand = {
   description: 'get a random number from 1-100',
   execute(message: Message) {
     let num: number = Math.random() * 100;
-    if (message.author.id === '151860528203956226') {
-      num = num / 10;
-    }
+
     num = Math.trunc(num);
     message.channel.send(`You rolled a ${num}`);
   },
